c21f224a1f7a32e4c63a4357b4798e91eff9dd29
Marta Neris <78042453+Martha-boop@users.noreply.github.com>
2022-04-13 15:53:35 -0300

    Add project files.

---
+++ Controle_de_Contatos_2/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Controle_de_Contatos_2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Controle_de_Contatos_2.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controle_de_Contatos_2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Controle_de_Contatos_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
