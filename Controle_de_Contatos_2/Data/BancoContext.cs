9dc78113817436c3e197b26823a4cff90b24d1d4
Marta Neris <78042453+Martha-boop@users.noreply.github.com>
2022-04-14 16:02:04 -0300

    commit

---
+++ Controle_de_Contatos_2/Data/BancoContext.cs
@@ -0,0 +1,15 @@
+﻿using Controle_de_Contatos_2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Controle_de_Contatos_2.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+        }
+        public DbSet<ContatoModel> Contatos { get; set; }
+
+    }
+}
+
