1b5f60ac68cfd4ef90d8badca62973248acc5e37
Marta Neris <78042453+Martha-boop@users.noreply.github.com>
2022-04-23 19:14:59 -0300

    commit

--- Controle_de_Contatos_2/Repositorio/IContatoRepositorio.cs
+++ Controle_de_Contatos_2/Repositorio/IContatoRepositorio.cs
@@ -4,8 +4,10 @@ namespace Controle_de_Contatos_2.Repositorio
 {
     public interface IContatoRepositorio
     {
+        ContatoModel ListarPorId(int id);
         List<ContatoModel> BuscarTodos();
         ContatoModel Adicionar(ContatoModel contato);
-        
+        ContatoModel Atualizar(ContatoModel contato);
+        bool Apagar(int id);
     }
 }
