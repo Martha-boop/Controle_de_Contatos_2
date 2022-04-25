3439fd334a44e46c47f8f7595057065b352d91ca
Marta Neris <78042453+Martha-boop@users.noreply.github.com>
2022-04-25 16:13:21 -0300

    commit

---
+++ Controle_de_Contatos_2/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using Controle_de_Contatos_2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Controle_de_Contatos_2.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Entrar(LoginModel loginModel)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                return View("Index");
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops! não conseguimos realizar seu login,tente novamente,detalhe do erro:{erro.Message}";
+                return RedirectToAction("Index");
+            }
+        }
+    }
+    
+  
+}
---
+++ Controle_de_Contatos_2/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Controle_de_Contatos_2.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Digite o Login")]
+        public string Login { get; set; }
+
+
+        [Required(ErrorMessage = "Digite a Senha")]
+        public string Senha { get; set; }
+    }
+}
