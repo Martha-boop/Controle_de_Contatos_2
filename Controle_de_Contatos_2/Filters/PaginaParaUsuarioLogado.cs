243fa32540d2ec2b8af9ffc361bee55760bbcc14
Marta Neris <78042453+Martha-boop@users.noreply.github.com>
2022-04-28 15:54:59 -0300

    commit

---
+++ Controle_de_Contatos_2/Filters/PaginaParaUsuarioLogado.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Controle_de_Contatos_2.Filters
+{
+    public class PaginaParaUsuarioLogado : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+        }
+        
+            
+        
+    }
+}
